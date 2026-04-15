210bc1a06508bd6ef7ff593db808b9c5521c63fe
meinie <929449726@qq.com>
2026-04-15 20:27:45 +0800

    Expand stage22 TMA plumbing

--- cute_attention/python_dsl/kernels/stage22_tma_backend.py
+++ cute_attention/python_dsl/kernels/stage22_tma_backend.py
@@ -99,6 +99,80 @@ class Stage22FlashAttentionTmaExperimental:
             defer_sync=True,
         )
 
+    def _make_kv_layouts(self):
+        smem_k_block_size = 64 if self._head_dim_padded % 64 == 0 else 32
+        swizzle_bits = 3 if smem_k_block_size == 64 else 2
+        sQ_layout_atom = cute.make_composed_layout(
+            cute.make_swizzle(swizzle_bits, 3, 3),
+            0,
+            cute.make_layout((8, smem_k_block_size), stride=(smem_k_block_size, 1)),
+        )
+        sQ_layout = cute.tile_to_shape(
+            sQ_layout_atom,
+            (self._m_block_size, self._head_dim_padded),
+            (0, 1),
+        )
+        sKV_layout = cute.tile_to_shape(
+            sQ_layout_atom,
+            (self._n_block_size, self._head_dim_padded),
+            (0, 1),
+        )
+        sKV_layout_staged = cute.tile_to_shape(
+            sQ_layout_atom,
+            (self._n_block_size, self._head_dim_padded, self._num_stages_kv),
+            (0, 1, 2),
+        )
+        return sQ_layout, sKV_layout, sKV_layout_staged
+
+    def _make_shared_storage_type(self, dtype, sQ_layout, sKV_layout_staged):
+        @cute.struct
+        class SharedStorage:
+            __annotations__ = {
+                "mainloop_pipeline_array_ptr": cute.struct.MemRange[cutlass.Int64, self._num_stages_kv],
+                "sQ": cute.struct.Align[cute.struct.MemRange[dtype, cute.cosize(sQ_layout)], 1024],
+                "sK": cute.struct.Align[cute.struct.MemRange[dtype, cute.cosize(sKV_layout_staged)], 1024],
+                "sV": cute.struct.Align[cute.struct.MemRange[dtype, cute.cosize(sKV_layout_staged)], 1024],
+            }
+
+        return SharedStorage
+
+    def _make_tma_kv_atoms_and_tensors(self, gK: cute.Tensor, gV: cute.Tensor, sKV_layout_staged):
+        tma_tile = (self._n_block_size, self._head_dim_padded)
+        tma_atom_k, tma_tensor_k = self._make_tma_atom_and_tensor(gK, sKV_layout_staged, tma_tile)
+        tma_atom_v, tma_tensor_v = self._make_tma_atom_and_tensor(gV, sKV_layout_staged, tma_tile)
+        return tma_atom_k, tma_tensor_k, tma_atom_v, tma_tensor_v
+
+    def _partition_tma_kv(self, tma_atom, tma_tensor, sTensor, gTensor):
+        s_for_tma_partition = cute.group_modes(sTensor, 0, 2)
+        g_for_tma_partition = cute.group_modes(gTensor, 0, 2)
+        cta_layout = cute.make_layout((1,))
+        cta_coord = 0
+        return cute.nvgpu.cpasync.tma_partition(
+            tma_atom,
+            cta_coord,
+            cta_layout,
+            s_for_tma_partition,
+            g_for_tma_partition,
+        )
+
+    @cute.jit
+    def _tma_prefetch_descriptors(self, tma_atom_k, tma_atom_v, warp_idx):
+        if warp_idx == 0:
+            cute.nvgpu.cpasync.prefetch_descriptor(tma_atom_k)
+            cute.nvgpu.cpasync.prefetch_descriptor(tma_atom_v)
+
+    @cute.jit
+    def _make_producer_state(self):
+        return pipeline.make_pipeline_state(
+            pipeline.PipelineUserType.Producer, self._num_stages_kv
+        )
+
+    @cute.jit
+    def _make_consumer_state(self):
+        return pipeline.make_pipeline_state(
+            pipeline.PipelineUserType.Consumer, self._num_stages_kv
+        )
+
     @cute.jit
     def __call__(
         self,
@@ -109,9 +183,33 @@ class Stage22FlashAttentionTmaExperimental:
         softmax_scale: cutlass.Float32,
         stream: cuda.CUstream,
     ):
-        # The backend is intentionally not launched yet. We still rely on the
-        # stage21 cp.async path for correctness/performance while stage22 grows
-        # real TMA load plumbing.
+        if cutlass.const_expr(not (mQ.element_type == mK.element_type == mV.element_type == mO.element_type)):
+            raise TypeError("All tensors must have the same data type")
+        if cutlass.const_expr(mQ.element_type != cutlass.Float16):
+            raise TypeError("Only Float16 is supported")
+
+        self._dtype = mQ.element_type
+        sQ_layout, sKV_layout, sKV_layout_staged = self._make_kv_layouts()
+        _ = sKV_layout  # carried for parity with stage21 shape plumbing
+
+        shared_storage = self._make_shared_storage_type(self._dtype, sQ_layout, sKV_layout_staged)
+        storage = cutlass.utils.SmemAllocator().allocate(shared_storage)
+        mainloop_pipeline_array_ptr = storage.mainloop_pipeline_array_ptr.data_ptr()
+        tx_count = cute.size_in_bytes(cute.slice_(sKV_layout_staged, (None, None, 0)))
+        _ = self._make_mainloop_pipeline(mainloop_pipeline_array_ptr, tx_count)
+
+        gK = cute.local_tile(mK[0, None, 0, None], (self._n_block_size, self._head_dim_padded), (None, 0))
+        gV = cute.local_tile(mV[0, None, 0, None], (self._n_block_size, self._head_dim_padded), (None, 0))
+        sK = storage.sK.get_tensor(sKV_layout_staged)
+        sV = storage.sV.get_tensor(sKV_layout_staged)
+        tma_atom_k, tma_tensor_k, tma_atom_v, tma_tensor_v = self._make_tma_kv_atoms_and_tensors(
+            gK, gV, sKV_layout_staged
+        )
+        _ = tma_tensor_k
+        _ = tma_tensor_v
+        _ = self._partition_tma_kv(tma_atom_k, tma_tensor_k, sK, gK)
+        _ = self._partition_tma_kv(tma_atom_v, tma_tensor_v, sV, gV)
+
         raise NotImplementedError(
-            "stage22_tma backend scaffold is not wired into a runnable kernel yet."
+            "stage22_tma now has real TMA atom/pipeline scaffolding, but the K/V mainloop load path is not wired into a runnable kernel yet."
         )
